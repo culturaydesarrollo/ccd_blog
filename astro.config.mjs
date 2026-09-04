127245dc2f399eff50bb02d61ae2e6ca766b1164
culturaydesarrollo <corporacion.cultura.y.desarrollo@gmail.com>
2026-09-04 01:45:53 -0500

    Update astro.config.mjs to remove base property
    Removed base property from Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,4 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://culturaydesarrollo.org',
-  base: '/blog', // <-- Esto es lo que le indica a la red dónde encajar los archivos compilados
 });
