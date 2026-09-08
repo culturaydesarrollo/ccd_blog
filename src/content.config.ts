3803cdd6cd74a6a0c20dd1b7bd4012cc084b9805
culturaydesarrollo <corporacion.cultura.y.desarrollo@gmail.com>
2026-09-08 14:02:14 -0500

    Change category from enum to array with default

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,7 +9,7 @@ const blog = defineCollection({
     pubDate: z.coerce.date(),
     image: z.string().optional(),
     author: z.string().default('Corporación Cultura y Desarrollo'),
-    category: z.enum(['Gestión Cultural', 'Comunidad', 'Proyectos', 'Patrimonio']),
+     category: z.array(z.string()).default([]),
     tags: z.array(z.string()).default([]),
   }),
 });
